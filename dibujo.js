b13783a3664e9d1161df46d92bf887fa70f44da2
Rafadinho <rafa85jogo@gmail.com>
2018-08-07 12:47:19 -0600

    agregando ciclo for y while

--- dibujo.js
+++ dibujo.js
@@ -1,9 +1,25 @@
 var d= document.getElementById("dibujito");
 var lienzo = d.getContext("2d");
-console.log(lienzo);
-lienzo.beginPath();//arrancar un trazo
-lienzo.strokeStyle = "blue";//escogimos el color
-lienzo.moveTo(100, 100);//nos movimos al punto 100 100
-lienzo.lineTo(200, 200);//creamos una linea diagional
-lienzo.stroke();//cerramos la linea
-lienzo.clossePath();//y terminamos de dibujar
+var linea=30;
+var l=0;
+var yi, xf;
+var colorcito = "blue";
+
+for(l = 0; l < linea; l++){
+  yi= 10 * l;
+  xf= 10 * (l+1);
+  dibujarLinea(colorcito,  0, yi, xf, 300);
+}
+dibujarLinea(colorcito, 1,1,1,299);
+dibujarLinea(colorcito, 1,299,299,299);
+
+function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
+{
+
+  lienzo.beginPath();
+  lienzo.strokeStyle = color;
+  lienzo.moveTo(xinicial, yinicial);
+  lienzo.lineTo(xfinal, yfinal);
+  lienzo.stroke();
+  lienzo.closePath();
+}
