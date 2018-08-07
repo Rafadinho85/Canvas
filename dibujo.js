53b6543941cfa9d7484acc3420c551270e89a1da
Rafadinho <rafa85jogo@gmail.com>
2018-08-06 21:42:09 -0600

    Terminado la practica de dibujo en DOM

--- dibujo.js
+++ dibujo.js
@@ -1,1 +1,9 @@
-alert("yo soy genial");
+var d= document.getElementById("dibujito");
+var lienzo = d.getContext("2d");
+console.log(lienzo);
+lienzo.beginPath();//arrancar un trazo
+lienzo.strokeStyle = "blue";//escogimos el color
+lienzo.moveTo(100, 100);//nos movimos al punto 100 100
+lienzo.lineTo(200, 200);//creamos una linea diagional
+lienzo.stroke();//cerramos la linea
+lienzo.clossePath();//y terminamos de dibujar
