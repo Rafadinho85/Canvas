c68f157d2416bf5365b61ff4d143053e39738376
Rafadinho <rafa85jogo@gmail.com>
2018-08-08 11:08:58 -0600

    agregando eventos y formulario

--- dibujo.js
+++ dibujo.js
@@ -1,13 +1,32 @@
+var texto = document.getElementById("texto_lineas");
+var boton = document.getElementById("botoncito");
+boton.addEventListener("click", dibujoPorClick);
+
 var d= document.getElementById("dibujito");
 var lienzo = d.getContext("2d");
-var linea=30;
+var ancho = d.width;
+
+function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
+{
+
+  lienzo.beginPath();
+  lienzo.strokeStyle = color;
+  lienzo.moveTo(xinicial, yinicial);
+  lienzo.lineTo(xfinal, yfinal);
+  lienzo.stroke();
+  lienzo.closePath();
+}
+
+function dibujoPorClick (){
+var linea=parseInt(texto.value);
 var l=0;
 var yi, xf;
 var colorcito = "blue";
+var espacio = ancho / linea;
 
 for(l = 0; l < linea; l++){
-  yi= 10 * l;
-  xf= 10 * (l+1);
+  yi= espacio * l;
+  xf= espacio * (l+1);
   dibujarLinea(colorcito,  0, yi, xf, 300);
   dibujarLinea(colorcito, 0, (300-yi), xf, 0);
   dibujarLinea(colorcito,  300, yi, (300-xf), 300);
@@ -19,13 +38,4 @@ dibujarLinea(colorcito, 1,299,299,299);
 dibujarLinea(colorcito, 299, 1, 299, 299);
 dibujarLinea(colorcito, 1, 1, 299, 1);
 
-function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
-{
-
-  lienzo.beginPath();
-  lienzo.strokeStyle = color;
-  lienzo.moveTo(xinicial, yinicial);
-  lienzo.lineTo(xfinal, yfinal);
-  lienzo.stroke();
-  lienzo.closePath();
 }
